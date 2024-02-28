4e0cc9955b2b24963d5afc7b0ca3392048c3504c
Dinh Kha Nguyen <82517228+nguyendinhkha@users.noreply.github.com>
2024-02-28 09:28:02 +0700

    Practical_exercises_03_GroupNingguang

---
+++ Practical_exercises_03_GroupNingguang/Bai2_Lab3/Bai3_Client/TCPCLient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai3_Client
+{
+    public partial class TCPCLient : Form
+    {
+        public TCPCLient()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTCPServer_Click(object sender, EventArgs e)
+        {
+            //Open new server
+            btnTCPServer server = new btnTCPServer();
+            server.Show();
+        }
+
+        private void btnTCPClient_Click(object sender, EventArgs e)
+        {
+            //Open new client
+            btnTCPClient client = new btnTCPClient();
+            client.Show();
+        }
+    }
+}
---
+++ Practical_exercises_03_GroupNingguang/Bai2_Lab3/Bai3_Client/btnTCPServer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Bai3_Client
+{
+    public partial class btnTCPServer : Form
+    {
+        public btnTCPServer()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListen_Click(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            Thread serverThread = new Thread(new ThreadStart(ServerListen));
+            serverThread.IsBackground = true;
+            serverThread.Start();
+        }
+
+        TcpListener Server = default(TcpListener);
+        TcpClient client = default(TcpClient);
+        NetworkStream stream = default(NetworkStream);
+
+        void ServerListen()
+        {
+            try
+            {
+                Server = new TcpListener(IPAddress.Parse("127.0.0.1"), 8080);
+                // Start listening for client requests.
+                Server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                //Stop to wait for request
+                client = Server.AcceptTcpClient();
+                AddMessage("Connected!");
+
+                stream = client.GetStream();
+
+                int i;
+
+                // Loop to receive all the data sent by the client.
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+
+                    AddMessage(data);
+                }
+
+                // Shutdown and end connection
+                stream.Close();
+                client.Close();
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x.ToString());
+            }
+        }
+
+        void AddMessage(string s)
+        {
+            view.Items.Add(new ListViewItem() { Text = s });
+        }
+
+        private void btnTCPServer_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Server.Stop();
+        }
+    }
+}
+
